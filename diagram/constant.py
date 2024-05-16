224837ffbbc6d8863cc6366ebb164fdd52992656
Sangmin SHIM <tkdals4723@gmail.com>
2024-05-16 19:48:21 +0200

    not that bad, but I think I could improve the qulaity and clarification!

--- diagram/constant.py
+++ diagram/constant.py
@@ -3,4 +3,9 @@ OUTPUT_FOLDER = 'output'
 
 CUSTOM_IMAGE_PATH = {
     "user" : "../assets/images/user.png",
+    "kafka" : "../assets/images/kafka.png",
+    "sentry" : "../assets/images/sentry.png",
+    "github" : "../assets/images/github.png",
+    "github_actions" : "../assets/images/github_actions.png",
+    "sonarqube" : "../assets/images/sonarqube.png",
 }
--- diagram/diagram.py
+++ diagram/diagram.py
@@ -1,12 +1,52 @@
-from diagrams import Diagram, Cluster
+from diagrams import Diagram, Cluster, Edge
 from diagrams.custom import Custom
 from constant import OUTPUT_FOLDER, CUSTOM_IMAGE_PATH
+from diagrams.onprem.compute import Server
+from diagrams.onprem.database import PostgreSQL
+from diagrams.onprem.network import Nginx
 
-with Diagram(f"NAME", show=False, filename="output/global_user_diagram"):
-    custom_user = Custom("USERS", CUSTOM_IMAGE_PATH["user"])
+# ----------------------------------------
+# Global User Diagram : User <->  3 APIs <-> 1 Broker Message Queue
+# ----------------------------------------
+with Diagram("Global", show=False, filename="output/global_user_diagram"):
+    nginx = Nginx("Nginx")
+    user = Custom("USERS", CUSTOM_IMAGE_PATH["user"])
+
+    with Cluster("Continuous Integration"):
+        github = Custom("GitHub", CUSTOM_IMAGE_PATH["github"])
+        github_actions = Custom("GitHub Actions", CUSTOM_IMAGE_PATH["github_actions"])
+        github >> github_actions
+        
+    with Cluster("Service Cluster"):    
+        with Cluster("API Services"):
+            db1 = PostgreSQL("Database1")
+            db2 = PostgreSQL("Database2")
+            db3 = PostgreSQL("Database3")
+            
+            apisvc = [
+                Server("Client"),
+                Server("Command"),
+                Server("Product")
+            ]
+
+            with Cluster("Database Cluster"):            
+                apisvc[0] >> db1
+                apisvc[1] >> db2
+                apisvc[2] >> db3
+                            
+            user>> nginx >> apisvc
+
+
+        with Cluster("Message Broker"):
+            kafka = Custom("Kafka", CUSTOM_IMAGE_PATH["kafka"])
+        
+        with Cluster("Monitoring"):    
+            sentry = Custom("Sentry", CUSTOM_IMAGE_PATH["sentry"])
+            
+            sentry << Edge(label="deployment", color="blue") >> github
+
     
     
-    with Cluster("User"):
-        custom_user >> custom_user
-        
-        
+    apisvc[1] >> kafka << Edge(label="logs", color="brown") >> sentry
+    
+    
