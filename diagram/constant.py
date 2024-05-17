19adbd14602cc1105d7eca887d086cd87d181913
Sangmin SHIM <tkdals4723@gmail.com>
2024-05-17 14:43:10 +0200

    user diagram + readMe

--- diagram/constant.py
+++ diagram/constant.py
@@ -2,5 +2,6 @@ OUTPUT_FOLDER = 'output'
 
 
 CUSTOM_IMAGE_PATH = {
+    "client" : "../assets/images/client.png",
     "sonarqube" : "../assets/images/sonarqube.png",
 }
--- diagram/diagrams/global_user_diagram.py
+++ diagram/diagrams/global_user_diagram.py
@@ -26,46 +26,103 @@ graph_attr = {
     "center": "true",
 }
 
+# ----------------------------------------
+# Define the diagram
+# ----------------------------------------
 with Diagram("Global",direction='TB', show=False, filename=f"{OUTPUT_FOLDER}/global_user_diagram", graph_attr=graph_attr) as diagram:
-    with Cluster("continuous_integration"):
+    
+    # ----------------------------------------
+    # 1) Cluster : continuous_integration
+    # ----------------------------------------
+    with Cluster("continuous_integration", graph_attr={"fontsize:":"20"}):
         github = Github("GitHub")
         github_actions = GithubActions("GitHub Actions")
 
+    # ----------------------------------------
+    # 2) Cluster : web_service
+    # ----------------------------------------
     with Cluster("web_service"):
+        
+        # ----------------------------------------
+        # 2-1) Cluster : api
+        # ----------------------------------------
         with Cluster("api"):
-            with Cluster("docker1"):
-                with Cluster("Client"):
+            
+            # ----------------------------------------
+            # 2-1-1) Cluster : docker_client
+            # ----------------------------------------
+            with Cluster("docker_client"):
+                
+                # ----------------------------------------
+                # 2-1-1-1) Cluster : Client
+                # ----------------------------------------
+                with Cluster("client"):
                     service_client = Server("Client")
                     db_client = PostgreSQL("Client Database")
-                    service_client >> db_client
-            with Cluster("docker2"):
-                with Cluster("Product"):
+                   
+                    
+            # ----------------------------------------
+            # 2-1-2) Cluster : docker_product
+            # ----------------------------------------
+            with Cluster("docker_product"):
+                
+                # ----------------------------------------
+                # 2-1-2-1) Cluster : Product
+                # ----------------------------------------
+                with Cluster("product"):
                     service_product = Server("Product")
                     db_product = PostgreSQL("Product Database")
-                    service_product >> db_product
-            with Cluster("docker3"):
-                with Cluster("Command"):
+                    
+            # ----------------------------------------
+            # 2-1-3) Cluster : docker_command
+            # ----------------------------------------
+            with Cluster("docker_command"):
+                
+                # ----------------------------------------
+                # 2-1-3-1) Cluster : Command
+                # ----------------------------------------
+                with Cluster("command"):
                     service_command = Server("Command")
                     db_command = PostgreSQL("Command Database")
-                    service_command >> db_command
+                    
 
-        with Cluster("sonarqube", graph_attr={"pencolor": "#60193C", "bgcolor": "#fefefe"}):
+        # ----------------------------------------
+        # 3) Cluster : sonarqube
+        # ----------------------------------------
+        with Cluster("code_quality", graph_attr={"pencolor": "#60193C", "bgcolor": "#fefefe"}):
             sonarqube = Custom("SonarQube", CUSTOM_IMAGE_PATH["sonarqube"])
 
+        # ----------------------------------------
+        # 4) Cluster : message_broker
+        # ----------------------------------------
         with Cluster("message_broker"):
             kafka = Kafka("Kafka")
 
+    # ----------------------------------------
+    # 5) Cluster : monitoring
+    # ----------------------------------------
     with Cluster("monitoring"):
         sentry = Sentry("Sentry")
 
+    # ----------------------------------------
+    # 6) Cluster : user
+    # ----------------------------------------
     with Cluster(label="client"):
-        client = User("Client")
+        client = Custom("Client", CUSTOM_IMAGE_PATH["client"])
         nginx = Nginx("Nginx")
-
-    client >> nginx << Edge(label="rest", color="purple", ltail="cluster_user", lhead="cluster_web_service") >> [service_command, service_product, service_client]
-    service_command << Edge(color="purple", ltail="cluster_api", lhead="cluster_sonarqube") >> sonarqube
-    service_command << Edge(ltail="cluster_api", lhead="cluster_message_broker") >> kafka
-    service_command << Edge(ltail="cluster_web_service", lhead="cluster_monitoring") >> sentry
+        
+    
+    # ----------------------------------------
+    # Define the connections
+    # ----------------------------------------
+    service_client >> db_client        
+    service_product >> db_product
+    service_command >> db_command
+    client >> nginx << Edge(label="API REST", color="purple", ltail="cluster_user", lhead="cluster_web_service") >> [service_product]
+    service_command << Edge(label="code quality", color="purple", ltail="cluster_api", lhead="cluster_code_quality") >> sonarqube
+    service_command << Edge(label="message", ltail="cluster_api", lhead="cluster_message_broker") >> kafka
+    service_command << Edge(label="monitoring", ltail="cluster_web_service", lhead="cluster_monitoring") >> sentry
     service_command << Edge(label="deployment", color="blue", ltail="cluster_web_service", lhead="cluster_continuous_integration") >> github >> github_actions
 
 diagram
+
